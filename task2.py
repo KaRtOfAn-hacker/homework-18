9116e35002000305cdf8544ede88a0054e405291
KaRtOfAn-hacker <yugin.sotnyk@gmail.com>
2026-06-23 17:23:52 +0200

    Delete task2.py

--- task2.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-df = sns.load_dataset('tips')
-sns.distplot(df['total_bill'], bins=20, kde=True, color='brown', alpha=0.7, linewidth=3)
-plt.title('Розподіл Сума Рахунку з Налаштуваннями Графіка')
-plt.xlabel('Сума Рахунку (USD)')
-plt.ylabel('Щільність Ймовірності')
-plt.show()
